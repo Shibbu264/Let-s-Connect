7f9b65e7ce08dcd644327923dc82a49330365e33
Shibbu264 <116422972+Shibbu264@users.noreply.github.com>
2024-06-02 17:22:59 +0530

    Update peer.js

--- hooks/peer.js
+++ hooks/peer.js
@@ -13,26 +13,26 @@ const usePeer = () => {
     const socket = useSocket()
     const roomId = useRouter().query.roomid
 
-    useEffect(() => {
-        if (peerset.current || !roomId || !socket) return;
-        peerset.current = true;
+    // useEffect(() => {
+    //     if (peerset.current || !roomId || !socket) return;
+    //     peerset.current = true;
 
-            import("peerjs").then(({ default: Peer }) => {
-                // normal synchronous code
-                const myPeer = new Peer({
-                    host: process.env.NEXT_PUBLIC_HOST,
-                    port: process.env.NEXT_PUBLIC_PORT,
-                    path: 'peerjs/myapp',
-                });
-                setPeer(myPeer)
-                myPeer.on('open', (id) => {
-                    socket?.emit('join-room', roomId, id)
-                    setMyId(id)
-                })
+    //         import("peerjs").then(({ default: Peer }) => {
+    //             // normal synchronous code
+    //             const myPeer = new Peer({
+    //                 host: process.env.NEXT_PUBLIC_HOST,
+    //                 port: process.env.NEXT_PUBLIC_PORT,
+    //                 path: 'peerjs/myapp',
+    //             });
+    //             setPeer(myPeer)
+    //             myPeer.on('open', (id) => {
+    //                 socket?.emit('join-room', roomId, id)
+    //                 setMyId(id)
+    //             })
 
-            })
+    //         })
 
-    }, [roomId, socket])
+    // }, [roomId, socket])
 
     return {
         peer, myId
